eab8ef472a0ca8610afff3fb0454987fd9cff8c8
Sriram <sriram1990usa@gmail.com>
2026-06-19 21:33:19 +0530

    Clean up temporary diagnostics views, middleware, URLs, and test logo template

--- premiumshop/settings.py
+++ premiumshop/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.views.ExceptionLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'premiumshop.urls'
